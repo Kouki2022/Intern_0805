95c2122c16e59d746b0d4293d7ede10edc799c88
Kage57 <eg5737021414@gmail.com>
2024-08-06 11:20:50 +0900

    “test”

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from './Home';
+import TopPage from './TopPage'; // または './Home' （ファイル名を変更しなかった場合）
 import RecipientList from './RecipientList';
 import HelloWorld from './HelloWorld';
 import CompletionScreen from './CompletionScreen';
+import Request from './Request'; 
 
 function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<TopPage />} />
       <Route path="/recipients" element={<RecipientList />} />
       <Route path="/send" element={<HelloWorld />} />
       <Route path="/completion" element={<CompletionScreen />} />
+      <Route path="/request" element={<Request />} /> 
+
     </Routes>
   );
 }
